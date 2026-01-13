41a1c57c487d7f43c8746811184c44b2ce6d0adf
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-01-13 13:53:33 +0100

    Add project files.

---
+++ NetCoreAdoNet/Forms/Form01PrimerAdo.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace NetCoreAdoNet.Forms
+{
+    partial class Form01PrimerAdo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnConectar = new Button();
+            btnDesconectar = new Button();
+            btnRead = new Button();
+            label1 = new Label();
+            lstApellidos = new ListBox();
+            lstColumnas = new ListBox();
+            label2 = new Label();
+            lstTipos = new ListBox();
+            label3 = new Label();
+            lblConexion = new Label();
+            SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            btnConectar.Location = new Point(26, 51);
+            btnConectar.Name = "btnConectar";
+            btnConectar.Size = new Size(80, 28);
+            btnConectar.TabIndex = 0;
+            btnConectar.Text = "Conectar";
+            btnConectar.UseVisualStyleBackColor = true;
+            btnConectar.Click += btnConectar_Click;
+            // 
+            // btnDesconectar
+            // 
+            btnDesconectar.Location = new Point(26, 85);
+            btnDesconectar.Name = "btnDesconectar";
+            btnDesconectar.Size = new Size(80, 28);
+            btnDesconectar.TabIndex = 1;
+            btnDesconectar.Text = "Desconectar";
+            btnDesconectar.UseVisualStyleBackColor = true;
+            btnDesconectar.Click += btnDesconectar_Click;
+            // 
+            // btnRead
+            // 
+            btnRead.Location = new Point(26, 119);
+            btnRead.Name = "btnRead";
+            btnRead.Size = new Size(80, 28);
+            btnRead.TabIndex = 2;
+            btnRead.Text = "Read";
+            btnRead.UseVisualStyleBackColor = true;
+            btnRead.Click += btnRead_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(124, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Apellidos";
+            // 
+            // lstApellidos
+            // 
+            lstApellidos.FormattingEnabled = true;
+            lstApellidos.Location = new Point(124, 52);
+            lstApellidos.Name = "lstApellidos";
+            lstApellidos.Size = new Size(120, 169);
+            lstApellidos.TabIndex = 4;
+            // 
+            // lstColumnas
+            // 
+            lstColumnas.FormattingEnabled = true;
+            lstColumnas.Location = new Point(266, 52);
+            lstColumnas.Name = "lstColumnas";
+            lstColumnas.Size = new Size(120, 169);
+            lstColumnas.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(266, 34);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Columnas";
+            // 
+            // lstTipos
+            // 
+            lstTipos.FormattingEnabled = true;
+            lstTipos.Location = new Point(403, 52);
+            lstTipos.Name = "lstTipos";
+            lstTipos.Size = new Size(120, 169);
+            lstTipos.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(403, 34);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Tipos Dato";
+            // 
+            // lblConexion
+            // 
+            lblConexion.AutoSize = true;
+            lblConexion.Location = new Point(26, 233);
+            lblConexion.Name = "lblConexion";
+            lblConexion.Size = new Size(71, 15);
+            lblConexion.TabIndex = 9;
+            lblConexion.Text = "lblConexion";
+            // 
+            // Form01PrimerAdo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblConexion);
+            Controls.Add(lstTipos);
+            Controls.Add(label3);
+            Controls.Add(lstColumnas);
+            Controls.Add(label2);
+            Controls.Add(lstApellidos);
+            Controls.Add(label1);
+            Controls.Add(btnRead);
+            Controls.Add(btnDesconectar);
+            Controls.Add(btnConectar);
+            Name = "Form01PrimerAdo";
+            Text = "Form01PrimerAdo";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnConectar;
+        private Button btnDesconectar;
+        private Button btnRead;
+        private Label label1;
+        private ListBox lstApellidos;
+        private ListBox lstColumnas;
+        private Label label2;
+        private ListBox lstTipos;
+        private Label label3;
+        private Label lblConexion;
+    }
+}
---
+++ NetCoreAdoNet/Forms/Form02BuscadorEmpleados.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreAdoNet.Forms
+{
+    public partial class Form02BuscadorEmpleados : Form
+    {
+        public Form02BuscadorEmpleados()
+        {
+            InitializeComponent();
+        }
+    }
+}
