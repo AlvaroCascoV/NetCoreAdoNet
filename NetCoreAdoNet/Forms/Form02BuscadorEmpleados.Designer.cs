94c7070c21af4f9601d1b7233a9c5d7d150d8ffc
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-01-13 14:16:19 +0100

    form 02 filtrado de datos

--- NetCoreAdoNet/Forms/Form02BuscadorEmpleados.Designer.cs
+++ NetCoreAdoNet/Forms/Form02BuscadorEmpleados.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form02BuscadorEmpleados";
+            label1 = new Label();
+            txtSalario = new TextBox();
+            btnBuscar = new Button();
+            label2 = new Label();
+            lstEmpleados = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(104, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Introduzca Salario:";
+            // 
+            // txtSalario
+            // 
+            txtSalario.Location = new Point(16, 34);
+            txtSalario.Name = "txtSalario";
+            txtSalario.PlaceholderText = "0";
+            txtSalario.Size = new Size(113, 23);
+            txtSalario.TabIndex = 1;
+            txtSalario.Text = "0";
+            // 
+            // btnBuscar
+            // 
+            btnBuscar.Location = new Point(12, 63);
+            btnBuscar.Name = "btnBuscar";
+            btnBuscar.Size = new Size(117, 23);
+            btnBuscar.TabIndex = 2;
+            btnBuscar.Text = "Buscar Empleados";
+            btnBuscar.UseVisualStyleBackColor = true;
+            btnBuscar.Click += btnBuscar_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 103);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Empleados";
+            // 
+            // lstEmpleados
+            // 
+            lstEmpleados.FormattingEnabled = true;
+            lstEmpleados.Location = new Point(12, 121);
+            lstEmpleados.Name = "lstEmpleados";
+            lstEmpleados.Size = new Size(156, 229);
+            lstEmpleados.TabIndex = 4;
+            // 
+            // Form02BuscadorEmpleados
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lstEmpleados);
+            Controls.Add(label2);
+            Controls.Add(btnBuscar);
+            Controls.Add(txtSalario);
+            Controls.Add(label1);
+            Name = "Form02BuscadorEmpleados";
+            Text = "Form02BuscadorEmpleados";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtSalario;
+        private Button btnBuscar;
+        private Label label2;
+        private ListBox lstEmpleados;
     }
 }
--- NetCoreAdoNet/Forms/Form02BuscadorEmpleados.cs
+++ NetCoreAdoNet/Forms/Form02BuscadorEmpleados.cs
@@ -5,14 +5,52 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace NetCoreAdoNet.Forms
 {
     public partial class Form02BuscadorEmpleados : Form
     {
+        //SOTA, CABALLO Y REY
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
         public Form02BuscadorEmpleados()
         {
             InitializeComponent();
+            string connectionString = @"Data Source=LOCALHOST\DEVELOPER;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Trust Server Certificate=True";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            //VAMOS A CONCATENAR, POR LO QUE NUESTRO SALARIO SERA UN STRING
+            string salario = this.txtSalario.Text;
+            //CONSULTA SQL
+            string sql = "select * from EMP where SALARIO >= " + salario;
+            //CONFIGURAMOS EL COMANDO
+            this.com.Connection = this.cn;
+            //TIPO DE CONSULTA
+            this.com.CommandType = CommandType.Text;
+            //LA CONSULTA
+            this.com.CommandText = sql;
+            //ENTRAR Y SALIR 
+            //ABRIMOS CONEXION
+            this.cn.Open();
+            //EJECUTAMOS LA CONSULTA
+            this.reader = this.com.ExecuteReader();
+            //DIBUJAMOS LOS DATOS
+            this.lstEmpleados.Items.Clear();
+            while (this.reader.Read())
+            {
+                string apellido = this.reader["APELLIDO"].ToString();
+                string sal = this.reader["SALARIO"].ToString();
+                this.lstEmpleados.Items.Add(apellido + " - " + sal);
+            }
+            //SALIMOS, LIBERAMOS RECURSOS
+            this.reader.Close();
+            this.cn.Close();
         }
     }
 }
