c029c579a2136b6e66527a5baae63dbe6d984915
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-01-19 10:22:11 +0100

    procedimientos almacenados

--- NetCoreAdoNet/Program.cs
+++ NetCoreAdoNet/Program.cs
@@ -23,7 +23,8 @@ namespace NetCoreAdoNet
             //Application.Run(new Form07DepartamentosEmpleadosPaco());
             //Application.Run(new Form08CrudDepartamentos());
             //Application.Run(new Form09CrudHospital());
-            Application.Run(new From10UpdateEmpleadosOficios());
+            //Application.Run(new From10UpdateEmpleadosOficios());
+            Application.Run(new Form11ProcedimientosHospitalPlantilla());
         }
     }
 }
